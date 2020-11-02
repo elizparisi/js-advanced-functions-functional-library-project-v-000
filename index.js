433a690603ab9a411d2bcec068446b1edac5843c
Liz P <parisielizabeth@gmail.com>
2020-11-02 05:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ const fi = (function() {
     //   return newArray;
     // },
     
-    uniq: function(array, boolean, element) {
+    uniq: function(array, sorted=false, element) {
       
     },
     
